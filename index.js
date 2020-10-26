c4f9c36b69bf8dd30e80174e5047fb2ee1b9bb9e
Tomer Gev <tgev@riparian.com>
2020-10-26 15:47:52 -0700

    Added more types to the determine type function

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 function determineType(value) {
   if (isNull(value)) return 'null'
+
   if (isObjectLike(value)) {
+    if (isArguments(value)) return 'arguments'
     if (isArray(value)) return 'array'
+    if (isDate(value)) return 'date'
     if (isMap(value)) return 'map'
     if (isObjectStrict(value)) return 'object'
     if (isSet(value)) return 'set'
-    if (isSymbol(value)) return 'symbol'
   }
   // check primitive types (exculding symbol and bigint)
   if (isBoolean(value)) return 'boolean'
+  if (isClass(value)) return 'class'
+  if (isFunction(value)) return 'function'
   if (isNumber(value)) return 'number'
   if (isString(value)) return 'string'
+  if (isSymbol(value)) return 'symbol'
   if (isUndefined(value)) return 'undefined'
+
+  if (isObjectLike(value)) return 'object-like'
 }
 
 function getTag(value) {
@@ -21,12 +28,24 @@ function getTag(value) {
   return Object.prototype.toString.call(value)
 }
 
+function isArguments(value) {
+  return isObjectLike(value) && getTag(value) == '[object Arguments]'
+}
 function isArray(value) {
   return Array.isArray(value)
 }
 function isBoolean(value) {
   return value === true || value === false ||
     (isObjectLike(value) && getTag(value) == '[object Boolean]')
+}
+function isClass(value) {
+  return typeof value === 'function' && /^\s*class\s+/.test(value.toString())
+}
+function isDate(value) {
+  return isObjectLike(value) && getTag(value) == '[object Date]'
+}
+function isFunction(value) {
+  return typeof value === 'function'
 }
 function isMap(value) {
   return isObjectLike(value) && getTag(value) == '[object Map]'
@@ -68,21 +87,32 @@ function test() {
 
   [
     {},
+    Object({}),
+    new Object({}),
     [],
+    new Array(2),
     0,
     1,
     NaN,
     Infinity,
+    new Number(1),
+    Foo,
     new Foo(),
     new Map([]),
     new Set([]),
-    // new Symbol([]),
+    Symbol('foo'),
     'Hello',
     '',
+    new String('Hello World'),
     undefined,
     null,
     true,
     false,
+    new Boolean(true),
+    () => {},
+    function () {},
+    arguments,
+    new Date(),
   ].forEach(function (val) {
     console.log(determineType(val))
   })
